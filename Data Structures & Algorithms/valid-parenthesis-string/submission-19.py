a21730dc4f7806fd9ddc425765a95dcffa761cc0
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-04-10 13:09:46 +0800

    Add: valid-parenthesis-string - submission-19

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-19.py
@@ -0,0 +1,26 @@
+# DP(TD, BU)
+# Greedy
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if not s:
+            return True        
+        # state: i and o
+        n = len(s)
+        dp = [[False] * (n + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        for i in range(1, n + 1):
+            for o in range(n):
+                if s[i - 1] == '(':
+                    if o > 0:
+                        dp[i][o] |= dp[i - 1][o - 1]
+                elif s[i - 1] == ')':
+                    if o < n:
+                        dp[i][o] |= dp[i - 1][o + 1]
+                else:
+                    if o > 0:
+                        dp[i][o] |= dp[i - 1][o - 1]
+                    if o < n:
+                        dp[i][o] |= dp[i - 1][o + 1]
+                    dp[i][o] |= dp[i - 1][o]
+                        
+        return dp[-1][0]
