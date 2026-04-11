143b9a8c4fb000221dcf2801f7f25954a8590b6e
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-04-11 16:36:03 +0800

    Add: count-number-of-islands - submission-16

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-16.py
@@ -0,0 +1,43 @@
+class DUS:
+    def __init__(self, N):
+        self.parent = list(range(N))
+        self.rank = [1] * N
+        self.count = 0
+
+    def find(self, i):
+        if i == self.parent[i]:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+    
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu != pv:
+            if self.rank[pv] < self.rank[pu]:
+                self.parent[pv] = pu
+                self.rank[pu] += self.rank[pv]
+            else:
+                self.parent[pu] = pv
+                self.rank[pv] += self.rank[pu]
+            self.count -= 1
+        
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        ROWS, COLS = len(grid), len(grid[0])
+        res = 0
+        dus = DUS(ROWS * COLS)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dus.count += 1
+                    this = r * COLS + c
+                    if r != ROWS - 1 and grid[r + 1][c] == "1":
+                        below = (r + 1) * COLS + c
+                        dus.union(this, below)
+                    if c != COLS -1 and grid[r][c + 1] == "1":
+                        right = r * COLS + c + 1
+                        dus.union(this, right)
+        return dus.count
