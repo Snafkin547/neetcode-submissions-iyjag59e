3d5c440c9112033226386d8b75e507a0c1a5082f
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-05-04 18:15:44 +0800

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if not node:
+                res.append("¥")
+            else:
+                res.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+        return '¥#'+'#'.join(res)
+
+    # Decodes your encoded data to tree.
+    def deserialize(self,data: str) -> Optional[TreeNode]:
+        # while q or first
+        # count length and work only on thise count
+        # append if not none
+        i = 0
+        dummy = TreeNode()
+        q = deque([dummy])
+        n = len(data)
+        while q:
+            size = len(q)
+            while size:
+                parent = q.popleft()
+                for isRight in range(2):
+                    r = i
+                    while r < n and data[r] != '#':
+                        r += 1
+                    
+                    val = data[i:r]
+                    
+                    if val == '¥':
+                        i = r + 1
+                        continue
+                    
+                    node = TreeNode(val = int(val))
+                    if isRight:
+                        parent.right = node
+                    else:
+                        parent.left = node
+                    q.append(node)
+                    i = r + 1
+                size -= 1
+        return dummy.right
+        
+
+
+
