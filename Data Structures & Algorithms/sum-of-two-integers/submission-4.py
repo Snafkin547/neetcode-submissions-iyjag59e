1d597f49a782ddb0eceb6c8ceaefaacd5d55ddc6
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-05-14 20:39:01 +0800

    Add: sum-of-two-integers - submission-4

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        res = carry = 0
+        mask = 2**32 - 1
+        limit = 2**31 - 1
+        
+        for i in range(32):
+            _a = (a >> i) & 1
+            _b = (b >> i) & 1
+            curr = _a ^ _b ^ carry # Returns 1 when 1 or 3 one(s)
+            carry = (_a & _b) | (_a & carry) | (_b & carry) # Returns 1 when 2 or 3 ones
+            if curr:
+                res |= (1 << i)
+        if res > limit:
+            res = ~(res ^ mask)
+        return res
+    # 3 Ones -> curr = 1, carry = 1 
+    # 2 Ones -> curr = 0, carry = 1
+    # 1 Ones -> curr = 1, carry = 0
+    # 0 Ones -> curr = 0, carry = 0
