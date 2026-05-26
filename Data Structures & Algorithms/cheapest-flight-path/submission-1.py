68a1c8e1be85574d3647472504977da0c2da095e
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-05-26 20:54:06 +0800

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        mp = defaultdict(list) # from: cost, to
+        for fm, to, cost in flights:
+            if fm == dst:
+                continue
+            mp[fm].append((to, cost))
+
+        visited = [float('inf')] * n # node: cost
+        q = deque([(src, 0)]) # node, cost
+
+        # bfs < k 
+        steps = -1
+        while steps <= k and q:
+            for i in range(len(q)):
+                node, ttl = q.popleft()
+                visited[node] = min(visited[node], ttl)
+                for nei, cost in mp[node]:
+                    if ttl + cost < visited[nei]:
+                        q.append((nei, ttl + cost))
+            steps += 1
+        return visited[dst] if visited[dst] != float('inf') else -1
