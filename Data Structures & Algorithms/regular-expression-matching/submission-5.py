42ffc51d86412553ffc06fdf16bd359c2c51e9ce
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-05-30 12:22:17 +0800

    Add: regular-expression-matching - submission-5

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        visited = {}
+
+        def dfs(i, j):
+
+            if (i, j) in visited:
+                return visited[(i, j)]
+
+            if j < 0:
+                return i < 0
+            
+            if i < 0:
+                while j >= 0 and p[j] == '*':
+                    j -= 2
+                return j < 0
+
+            visited[(i, j)] = False
+            # normal character match
+            if p[j] == s[i] or p[j] == '.':
+                visited[(i, j)] = dfs(i - 1, j - 1)
+
+            elif p[j] == '*':
+                visited[(i, j)] |= dfs(i, j - 2) # Entirely skipping wild card
+                if p[j - 1] == '.' or p[j - 1] == s[i]:
+                    visited[(i, j)] |= dfs(i - 1, j) # j/* stays here while checking i forward
+
+            return visited[(i, j)]
+        return dfs(len(s)-1, len(p)-1)
