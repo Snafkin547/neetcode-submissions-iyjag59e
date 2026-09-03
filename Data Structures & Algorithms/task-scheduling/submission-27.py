ec61199a0aea7f1c819e0f7179069bf0360d6984
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2026-09-03 13:27:54 +0800

    Add: task-scheduling - submission-27

---
+++ Data Structures & Algorithms/task-scheduling/submission-27.py
@@ -0,0 +1,34 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        curr = 0
+        taskq = [] # -bal, cy
+        waitq = [] # cy, -bal
+
+        _tasks = Counter(tasks)
+        for _, v in _tasks.items():
+            heapq.heappush(taskq, (-v, 0))
+
+        while taskq or waitq:
+            if not waitq and taskq:
+                bal, cy = heapq.heappop(taskq)
+                curr = max(curr + 1, cy)
+                if bal < -1:
+                    heapq.heappush(waitq, (curr + n + 1, bal + 1))
+            elif waitq and not taskq:
+                cy, bal = heapq.heappop(waitq)
+                curr = max(curr + 1, cy)
+                if bal < -1:
+                    heapq.heappush(waitq, (curr + n + 1, bal + 1))
+            else:
+                if waitq[0][1] < taskq[0][0] and waitq[0][0] <= curr + 1:
+                    cy, bal = heapq.heappop(waitq)
+                    curr += 1
+                    if bal < -1:
+                        heapq.heappush(waitq, (curr + 1 + n, bal + 1))
+
+                else:
+                    bal, cy = heapq.heappop(taskq)
+                    curr += 1
+                    if bal < -1:
+                        heapq.heappush(waitq, (curr + 1 + n, bal + 1))
+        return curr
